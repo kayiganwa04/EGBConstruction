d206186d1443b1cfc1adb069fe50ac00082e83f8
RubagumyaAlain <rubagumyaalainyeye126@gmail.com>
2025-12-29 22:11:01 +0200

    added Miduha project

--- components/ongoing-projects.tsx
+++ components/ongoing-projects.tsx
@@ -14,6 +14,7 @@ const OngoingProjects: React.FC = () => {
     { src: '/projects/residential3.png', alt: 'Ongoing Project 2' },
     { src: '/projects/Picture71.jpg', alt: 'Ongoing Project 3' },
     { src: '/projects/hospital1.jpeg', alt: 'Ongoing Project 4' },
+    { src: '/projects/Miduha.JPG', alt: 'Ongoing Project 5' },
   ]
 
   return (
