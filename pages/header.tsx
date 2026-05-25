b2ca5f99c9caf2c27d621cf19a6d08be9f5d48c2
RubagumyaAlain <rubagumyaalainyeye126@gmail.com>
2026-05-25 15:24:19 +0200

    photos update

---
+++ pages/header.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'
+import Nav from '../components/nav'
+import Header from '../components/header'
+import DreamHome from '../components/dream-home'
+import About from '../components/about'
+import Partners from '../components/partners'
+import Service from '../components/service'
+import Footer from '../components/footer'
+import AccomplishedPojects from '../components/accomplished-projects'
+import OnGoingProjects from '../components/ongoing-projects'
+import { Divider } from '@chakra-ui/core'
+
+const HeaderPage: React.FC = () => {
+  useEffect(() => {
+    const el = document.getElementById('header')
+    if (el) {
+      // header is at top; scroll to top smoothly
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
+  }, [])
+
+  return (
+    <>
+      <Nav />
+      <Header />
+      <DreamHome />
+      <About />
+      <Partners />
+      <Service />
+      <AccomplishedPojects />
+      <OnGoingProjects />
+      <Divider mt="24" />
+      <Footer />
+    </>
+  )
+}
+
+export default HeaderPage
---
+++ pages/service.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import Nav from '../components/nav'
+import Header from '../components/header'
+import DreamHome from '../components/dream-home'
+import About from '../components/about'
+import Partners from '../components/partners'
+import Service from '../components/service'
+import Footer from '../components/footer'
+import AccomplishedPojects from '../components/accomplished-projects'
+import OnGoingProjects from '../components/ongoing-projects'
+import { Divider } from '@chakra-ui/core'
+
+const ServicePage: React.FC = () => {
+  useEffect(() => {
+    const el = document.getElementById('service')
+    if (el) {
+      const navbar = document.querySelector('nav')
+      const navbarHeight = navbar ? navbar.clientHeight : 70
+      const targetPosition = el.getBoundingClientRect().top + window.pageYOffset - navbarHeight
+      window.scrollTo({ top: targetPosition, behavior: 'smooth' })
+    }
+  }, [])
+
+  return (
+    <>
+      <Nav />
+      <Header />
+      <DreamHome />
+      <About />
+      <Partners />
+      <Service />
+      <AccomplishedPojects />
+      <OnGoingProjects />
+      <Divider mt="24" />
+      <Footer />
+    </>
+  )
+}
+
+export default ServicePage
